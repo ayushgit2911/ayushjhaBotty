f178f0e81b3c32569f8dd769c87b3152bff04585
Ayush Kumar Jha <58726551+ayushjha2911@users.noreply.github.com>
2019-12-23 11:03:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Warning: Use this repo at your own risk
+
+#click_the_photo_below_to_deploy
+
+[![Deploy](https://github.com/sushantgirdhar/imagesgraa/blob/master/photo_2019-11-15_22-08-18.jpg)](https://heroku.com/deploy)
+
+
---
+++ stdplugins/alive.py
@@ -0,0 +1,22 @@
+""".alive Plugin for @UniBorg"""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd("alive"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "I'm alive and Working Fine ! My Master Ayush_jha\n\nTelethon version: Latest\nPython: Latest\nUser: @ayushjha_2911\nDatabase Status: AYUSH JHA's database functioning normally!"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
